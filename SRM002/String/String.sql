22690bb2767cb5d288e25240c42d406bb3f2fcaf
Gennovas System Co., Ltd. <122245317+gennovas@users.noreply.github.com>
2026-06-27 14:36:25 +0700

    remove insert description

--- SRM002/String/String.sql
+++ SRM002/String/String.sql
@@ -1,4 +1,11 @@
 SET NOCOUNT ON
 GO
 
-insert into strings values ('fServiceOrders', '1', '[NULL]', '(GSK) Service Orders', NULL, NULL, NULL, NULL)
+/*
+    Issues 
+    AppDemo 9.00.20 - ไม่มีฟิลด์ Description
+*/
+if not exists (select 1 from strings where ScopeType = '1' and name = 'fServiceOrders')
+    insert into strings values ('fServiceOrders', '1', '[NULL]', '(GSK) Service Orders', NULL, NULL, NULL
+--, NULL
+)
