880eb1a0e3a0e27b87b1ca2f43c13ee975be6d79
Gennovas System Co., Ltd. <122245317+gennovas@users.noreply.github.com>
2026-06-29 10:32:28 +0700

    change sql statement

--- SRM002/String/String.sql
+++ SRM002/String/String.sql
@@ -6,6 +6,5 @@ GO
     AppDemo 9.00.20 - ไม่มีฟิลด์ Description
 */
 if not exists (select 1 from strings where ScopeType = '1' and name = 'fServiceOrders')
-    insert into strings values ('fServiceOrders', '1', '[NULL]', '(GSK) Service Orders', NULL, NULL, NULL
---, NULL
+    insert into strings values ('fServiceOrders', '1', '[NULL]', '(GSK) Service Orders', NULL, NULL, NULL, NULL
 )
